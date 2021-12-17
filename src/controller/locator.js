a36f2b3d31c9a593bf150d5c3962405961372998
Heather Lloyd-Cunningham <heather.lloydcunningham@engineering.digital.dwp.gov.uk>
2021-12-17 12:43:10 +0000

    feat(validation): add validation for city and distance params

--- src/controller/locator.js
+++ src/controller/locator.js
@@ -1,7 +1,33 @@
+const joi = require('joi');
+const logger = require('../logger').getLogger();
+
+const validateParams = (params) => {
+  const schema = joi.object({
+    city: joi.string()
+      .pattern(/^([a-z]|[A-Z])+$/, 'letters')
+      .custom((str) => str.charAt(0).toUpperCase() + str.substr(1).toLowerCase(), 'title')
+      .required(),
+    distance: joi.number().min(0).required(),
+  });
+  return schema.validate(params, { stripUnknown: true, abortEarly: false });
+};
+
 const locateUsers = (req, res) => {
-  res.status(200).send('ok');
+  try {
+    logger.info('Received request to get users');
+    const validParams = validateParams(req.params);
+    if (validParams.error) {
+      logger.info(`Invalid parameters: ${validParams.error.message}`);
+      return res.status(400).send(validParams.error.message);
+    }
+    return res.status(200).send('ok');
+  } catch (err) {
+    logger.error(err.message);
+    return res.status(500).send('Internal Server Error');
+  }
 };
 
 module.exports = {
+  validateParams,
   locateUsers,
 };
--- test/unit/controller/locator.test.js
+++ test/unit/controller/locator.test.js
@@ -1,27 +1,144 @@
-/* eslint-disable no-undef */
+/* eslint-disable  no-undef, no-unused-expressions, no-underscore-dangle */
 const rewire = require('rewire');
 const chai = require('chai');
 const sinon = require('sinon');
 
 const { expect } = chai;
-
 describe('locator', () => {
-  describe('response is successful', () => {
-    const stubSend = sinon.stub().returns(true);
-    const stubStatus = sinon.stub().returns({ send: stubSend });
-    const fakeReq = { body: [] };
-    const fakeRes = { status: stubStatus };
-
-    const locator = rewire('../../../src/controller/locator');
-
-    it('will return not return anything', async () => {
-      await expect(locator.locateUsers(fakeReq, fakeRes)).to.be.undefined;
+  describe('validateParams', () => {
+    const { validateParams } = rewire('../../../src/controller/locator');
+    it('will not return errors for valid parameters and strip extras', () => {
+      const params = { city: 'London', distance: 10, unknown: true };
+      const result = validateParams(params);
+      expect(result).to.not.ownProperty('error');
+      expect(result.value).to.eql({ city: 'London', distance: 10 });
     });
-    it('Will Call status once', () => {
-      expect(stubStatus).to.be.calledOnceWith(200);
+    it('will convert city to title case and distance to a number', () => {
+      const params = { city: 'london', distance: '10' };
+      const result = validateParams(params);
+      expect(result).to.not.ownProperty('error');
+      expect(result.value).to.eql({ city: 'London', distance: 10 });
     });
-    it('Will Call send once', () => {
-      expect(stubSend).to.be.calledOnceWith('ok');
+    it('will error when params is empty', () => {
+      const params = {};
+      const result = validateParams(params);
+      expect(result).to.ownProperty('error');
+      expect(result.error.message).to.eql('"city" is required. "distance" is required');
+    });
+    it('will error when distance is less than 0', () => {
+      const params = { city: 'london', distance: -1 };
+      const result = validateParams(params);
+      expect(result).to.ownProperty('error');
+      expect(result.error.message).to.eql('"distance" must be greater than or equal to 0');
+    });
+    it('will error the city is invalid', () => {
+      const params = { city: 'new york', distance: 0 };
+      const result = validateParams(params);
+      expect(result).to.ownProperty('error');
+      expect(result.error.message).to.eql('"city" with value "new york" fails to match the letters pattern');
+    });
+  });
+  describe('locateUsers', () => {
+    describe('fails validation', () => {
+      const stubSend = sinon.stub().returns('sent');
+      const stubStatus = sinon.stub().returns({ send: stubSend });
+      const fakeParams = { city: 'new york', distance: 0 };
+      const fakeReq = { params: fakeParams };
+      const fakeRes = { status: stubStatus };
+      const stubValidateParams = sinon.stub().returns({ error: { message: 'fails validation' } });
+      const fakeLogger = { info: sinon.stub(), error: sinon.stub() };
+
+      const locator = rewire('../../../src/controller/locator');
+      locator.__set__('validateParams', stubValidateParams);
+      locator.__set__('logger', fakeLogger);
+
+      it('will return sent', async () => {
+        await expect(locator.locateUsers(fakeReq, fakeRes)).to.eql('sent');
+      });
+      it('Will call validateParams once', () => {
+        expect(stubValidateParams).to.be.calledOnceWith(fakeParams);
+      });
+      it('Will call status once', () => {
+        expect(stubStatus).to.be.calledOnceWith(400);
+      });
+      it('Will call send once', () => {
+        expect(stubSend).to.be.calledOnceWith('fails validation');
+      });
+      it('Will log information', () => {
+        expect(fakeLogger.info).to.be.calledTwice;
+        expect(fakeLogger.info.args[0][0]).to.eql('Received request to get users');
+        expect(fakeLogger.info.args[1][0]).to.eql('Invalid parameters: fails validation');
+      });
+      it('Will not log errors', () => {
+        expect(fakeLogger.error).to.not.be.called;
+      });
+    });
+    describe('response is successful', () => {
+      const stubSend = sinon.stub().returns('sent');
+      const stubStatus = sinon.stub().returns({ send: stubSend });
+      const fakeParams = { city: 'new york', distance: 0 };
+      const fakeReq = { params: fakeParams };
+      const fakeRes = { status: stubStatus };
+      const stubValidateParams = sinon.stub().returns({ value: fakeParams });
+      const fakeLogger = { info: sinon.stub(), error: sinon.stub() };
+
+      const locator = rewire('../../../src/controller/locator');
+      locator.__set__('validateParams', stubValidateParams);
+      locator.__set__('logger', fakeLogger);
+
+      it('will return sent', async () => {
+        await expect(locator.locateUsers(fakeReq, fakeRes)).to.eql('sent');
+      });
+      it('Will call validateParams once', () => {
+        expect(stubValidateParams).to.be.calledOnceWith(fakeParams);
+      });
+      it('Will call status once', () => {
+        expect(stubStatus).to.be.calledOnceWith(200);
+      });
+      it('Will call send once', () => {
+        expect(stubSend).to.be.calledOnceWith('ok');
+      });
+      it('Will log information', () => {
+        expect(fakeLogger.info).to.be.calledOnce;
+        expect(fakeLogger.info.args[0][0]).to.eql('Received request to get users');
+      });
+      it('Will not log errors', () => {
+        expect(fakeLogger.error).to.not.be.called;
+      });
+    });
+    describe('error occurs', () => {
+      const fakeError = new Error('unable to validate');
+      const stubSend = sinon.stub().returns('sent');
+      const stubStatus = sinon.stub().returns({ send: stubSend });
+      const fakeParams = { city: 'new york', distance: 0 };
+      const fakeReq = { params: fakeParams };
+      const fakeRes = { status: stubStatus };
+      const stubValidateParams = sinon.stub().throws(fakeError);
+      const fakeLogger = { info: sinon.stub(), error: sinon.stub() };
+
+      const locator = rewire('../../../src/controller/locator');
+      locator.__set__('validateParams', stubValidateParams);
+      locator.__set__('logger', fakeLogger);
+
+      it('will return sent', async () => {
+        await expect(locator.locateUsers(fakeReq, fakeRes)).to.eql('sent');
+      });
+      it('Will call validateParams once', () => {
+        expect(stubValidateParams).to.be.calledOnceWith(fakeParams);
+      });
+      it('Will call status once', () => {
+        expect(stubStatus).to.be.calledOnceWith(500);
+      });
+      it('Will call send once', () => {
+        expect(stubSend).to.be.calledOnceWith('Internal Server Error');
+      });
+      it('Will log information', () => {
+        expect(fakeLogger.info).to.be.calledOnce;
+        expect(fakeLogger.info.args[0][0]).to.eql('Received request to get users');
+      });
+      it('Will log errors', () => {
+        expect(fakeLogger.error).to.be.calledOnceWith('unable to validate');
+      });
     });
   });
 });
